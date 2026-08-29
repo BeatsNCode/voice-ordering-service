501088bb9bbd02e1fcf21209d9f87534593df175
Jean Augustin <jre.augustin@gmail.com>
2026-08-29 01:02:18 -0400

    Added Menu to homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,49 @@
 import { useEffect, useState } from 'react'
 
+type MenuItem = {
+  name: string
+  description: string
+  price: number
+}
+
 function App() {
+  const [menu, setMenu] = useState<MenuItem[]>([])
 
-  const [menu, setMenu] = useState([]);
-
-  const loadMenu = async () => {
-    try {
-      const response = await fetch('/api/');
-      const data = await response.json();
-      setMenu(data);
-      console.log('Menu loaded:', data);
-    } catch (error) {
-      console.error('Error loading menu:', error);
-    }
-  }
-  
   useEffect(() => {
-    loadMenu();
-  }, []);
-  
+    let isMounted = true
+
+    const loadMenu = async () => {
+      try {
+        const response = await fetch('/api/')
+        const data: MenuItem[] = await response.json()
+
+        if (isMounted) {
+          setMenu(data)
+          console.log('Menu loaded:', data)
+        }
+      } catch (error) {
+        console.error('Error loading menu:', error)
+      }
+    }
+
+    // void loadMenu()
+
+    return () => {
+      isMounted = false
+    }
+  }, [])
+
   return (
     <>
-      <h1>Voice Ordering Service</h1>
-      
+      <h1 style={{ paddingTop: '20px', paddingBottom: '10px' }}>Voice Ordering Service</h1>
+
+      {menu.map((item, index) => (
+        <div style={{ paddingTop: '5px' }} key={index} className="menu-item">
+          <h2>{item.name}</h2>
+          <p>{item.description}</p>
+          <p>Price: ${item.price}</p>
+        </div>
+      ))}
     </>
   )
 }

bb9995abae38732433a764f93e58051855fa5314
Jean Augustin <jre.augustin@gmail.com>
2026-09-07 15:18:07 -0400

    Refactored speech logic to account for item quantities and all possible combincations

--- client/src/components/menu.tsx
+++ client/src/components/menu.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Microphone from './microphone';
 import ShoppingCart from './shoppingCart';
 import type { OrderResult } from '../types/order';
+import { calculateOrderTotal } from '../types/order';
 import { playSpeech, SpeechLogic } from './playSpeech'
 
 type MenuItem = {
@@ -53,10 +54,9 @@ function Menu() {
 
   const dessertItems = menu.filter(item => item.category === 'dessert')
 
-  const orderTotal = order?.availableItems.reduce(
-    (total, item) => total + item.price * item.quantity,
-    0
-  ) ?? 0
+  const orderTotal = order
+  ? calculateOrderTotal(order)
+  : 0
 
   const handleOrderReceived = async (result: OrderResult) => {
     setOrder(result)
